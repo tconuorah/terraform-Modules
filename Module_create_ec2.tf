2f1bedfcf4ab7cb1c82c415e999bd475da5dd356
tconuorah <163480467+tconuorah@users.noreply.github.com>
2025-08-26 02:43:05 -0500

    Delete Module_create_ec2.tf

--- Module_create_ec2.tf
+++
@@ -1,6 +0,0 @@
-module "my_ec2_instance" {
-  source        = "./modules/ec2"
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t3.micro"
-  name          = "Terraform-Module-Instance"
-}
